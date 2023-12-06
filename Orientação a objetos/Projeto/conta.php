454d6977c0a8f4ff79d472d0747bf33f05eb76a7
Pablo <pablomoura2016@gmail.com>
2023-12-05 21:05:15 -0300

    first repositorio

--- Orientação a objetos/Projeto/conta.php
+++ Orientação a objetos/Projeto/conta.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<body> oi
     <?php
         class Conta{
             public $cpf;
@@ -28,7 +28,7 @@
         $segundaConta->saldo = 1500;
 
         echo "$umaConta";
-
+    
     ?>
 </body>
 </html>
