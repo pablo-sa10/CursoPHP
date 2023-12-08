09a0339f687fee6836686521268fdbf8bfbdd076
Pablo Roberto <pablomoura2016@gmail.com>
2023-12-08 14:41:10 -0300

    organizando as pastas

--- Orientação a objetos/projeto da Alura/src/Modelo/Funcionario/Funcionario.php
+++ Orientação a objetos/projeto da Alura/src/Modelo/Funcionario/Funcionario.php
@@ -7,19 +7,12 @@ use Alura\Banco\Modelo\Pessoa;
 
 abstract class Funcionario extends Pessoa
 {
-    private $cargo;
     private $salario;
 
-    public function __construct(string $nome, CPF $cpf, string $cargo, float $salario)
+    public function __construct(string $nome, CPF $cpf, float $salario)
     {
         parent::__construct($nome, $cpf);
-        $this->cargo = $cargo;
         $this->salario = $salario;
-    }
-
-    public function recuperaCargo(): string
-    {
-        return $this->cargo;
     }
 
     public function alteraNome(string $nome): void
@@ -41,10 +34,5 @@ abstract class Funcionario extends Pessoa
     public function recuperaSalario(): float
     {
         return $this->salario;
-    }
-
-    public function calculaBonificacao(): float
-    {
-        return $this->salario * 0.1;
     }
 }
